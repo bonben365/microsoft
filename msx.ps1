4d5b282cc9c5de289b6768f2c5cca88d892b3a92
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-03-13 09:10:59 +0700

    Update msx.ps1

--- msx.ps1
+++ msx.ps1
@@ -1,45 +1,42 @@
 $MainMenu = {
-    Write-Host " ******************************************************"
-    Write-Host " * Author: https://msgang.com                         *" 
-    Write-Host " * Updated on: 10/02/2023                             *" 
-    Write-Host " ******************************************************" 
-    Write-Host " 1. Professional (Activate and Convert)" 
-    Write-Host " 2. Enterprise (Activate and Convert)" 
-    Write-Host " 3. ProfessionalWorkstation (Activate and Convert)" 
-    Write-Host " 4. Education (Activate and Convert)" 
-    Write-Host " ******************************************************" 
-    Write-Host " 5. Windows Server" 
-    Write-Host " ******************************************************" 
-    Write-Host " Information:"
-    Write-Host "  - This script works on both Windows 10 and Windows 11."
-    Write-Host "  - You can activate or switch Windows edition."
-    Write-Host "  - Press Ctrl + C to Quit."
-    Write-Host " ******************************************************" 
+    Write-Host " ********************************************************************" -ForegroundColor Yellow
+    Write-Host " * Author: https://msgang.com                                       *" -ForegroundColor Yellow
+    Write-Host " * Updated on: 10/02/2023                                           *" -ForegroundColor Yellow
+    Write-Host " ********************************************************************" -ForegroundColor Yellow
+    Write-Host " 1. Professional (Activate and Convert)" -ForegroundColor Yellow
+    Write-Host " 2. Enterprise (Activate and Convert)" -ForegroundColor Yellow
+    Write-Host " 3. ProfessionalWorkstation (Activate and Convert)" -ForegroundColor Yellow
+    Write-Host " 4. Education (Activate and Convert)" -ForegroundColor Yellow
+    Write-Host " ********************************************************************" -ForegroundColor Yellow
+    Write-Host " 5. Windows Server 2016/2019/2022" -ForegroundColor Yellow
+    Write-Host " ********************************************************************" -ForegroundColor Yellow
+    Write-Host " Information:"-ForegroundColor Yellow
+    Write-Host
+    Write-Host "  - This script works on both Windows 10 and Windows 11."-ForegroundColor Yellow
+    Write-Host "  - You can activate, upgrade, downgrade or convert Windows editions."-ForegroundColor Yellow
+    Write-Host "  - Press Ctrl + C to Quit."-ForegroundColor Yellow
+    Write-Host " ********************************************************************" -ForegroundColor Yellow
     Write-Host 
-    Write-Host " Select an option and press Enter: "  -nonewline
+    Write-Host " Select an option and press Enter: "  -nonewline -ForegroundColor Green
     }
     cls
 
 $MenuServer = {
-    Write-Host " *******************************************"
-    Write-Host " *                  Menu                   *" 
-    Write-Host " *******************************************" 
-    Write-Host " 1. Standard | Windows Server 2016" 
-    Write-Host " 2. Standard | Windows Server 2019" 
-    Write-Host " 3. Standard | Windows Server 2022" 
-    Write-Host " *******************************************" 
-    Write-Host " 4. Datacenter | Windows Server 2016" 
-    Write-Host " 5. Datacenter | Windows Server 2019" 
-    Write-Host " 6. Datacenter | Windows Server 2022" 
-    Write-Host " 7. Quit"
+    Write-Host " *******************************************" -ForegroundColor Yellow
+    Write-Host " *                  Menu                   *" -ForegroundColor Yellow
+    Write-Host " *******************************************" -ForegroundColor Yellow
+    Write-Host " 1. Windows Server 2016" -ForegroundColor Yellow
+    Write-Host " 2. Windows Server 2019" -ForegroundColor Yellow
+    Write-Host " 3. Windows Server 2022" -ForegroundColor Yellow
+    Write-Host " 4. Quit" -ForegroundColor Yellow
     Write-Host 
-    Write-Host " Select an option and press Enter: "  -nonewline
+    Write-Host " Select an option and press Enter: " -ForegroundColor Green
     }
     
 $windows = {
 
     Write-Host
-    Write-Host " Processing............"
+    Write-Host " Processing............" -ForegroundColor Green
     $null = New-Item -Path $env:temp\kms -ItemType Directory -Force
     Set-Location $env:temp\kms
     $uri = "https://s3.amazonaws.com/s3.bonben365.com/files/zip/windows-converter/windows10/$edition.zip"
@@ -75,20 +72,22 @@ $windows = {
 $windowsserver = {
 
     Write-Host
-    Write-Host " Processing............"
+    Write-Host " Your Windows edition: $((Get-ComputerInfo).WindowsProductName)" -ForegroundColor Green
+    Write-Host
+    Write-Host " Processing............" -ForegroundColor Green
     Set-Location "$env:windir\system32"
     $null = cmd.exe /c cscript.exe slmgr.vbs /rilc
     $null = cmd.exe /c cscript.exe slmgr.vbs /upk
     $null = cmd.exe /c cscript.exe slmgr.vbs /ckms
     $null = cmd.exe /c cscript.exe slmgr.vbs /cpky
     $null = cmd.exe /c cscript.exe slmgr.vbs /skms kms.msgang.com
-    $null = cmd.exe /c cscript.exe slmgr.vbs /ipk $productkeystd
-    $null = cmd.exe /c cscript.exe slmgr.vbs /ipk $productkeydc
+    $null = cmd.exe /c cscript.exe slmgr.vbs /ipk $productkey
     $null = cmd.exe /c cscript.exe slmgr.vbs /ato
     Write-Host
-    Write-Host " Done............"
+    Write-Host " Done............" -ForegroundColor Green
     Write-Host
-    Write-Host " Your Windows edition: $((Get-ComputerInfo).WindowsProductName)"
+    Write-Host " License information:" -ForegroundColor Green
+    Write-Host
     $licStatus = cmd.exe /c cscript.exe slmgr.vbs /dlv | find "License Status"
     $actid = cmd.exe /c cscript.exe slmgr.vbs /dlv | find "Activation ID"
     $pkc = cmd.exe /c cscript.exe slmgr.vbs /dlv | find "Product Key Channel"
@@ -121,25 +120,24 @@ $windowsserver = {
                     cls
                     Invoke-Command $MenuServer
                     $selectServer = Read-Host
+
+                    $edition = (Get-ComputerInfo).WindowsEditionId
   
-                    if ($selectServer -eq 1) {$productkeystd = 'WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY';$productkeydc = 'CB7KF-BWN84-R7R2Y-793K2-8XDDG'} #Windows Server 2016 Standard
-                    if ($selectServer -eq 2) {$productkey = 'N69G4-B89J2-4G8F4-WWYCC-J464C'} #Windows Server 2019 Standard
-                    if ($selectServer -eq 3) {$productkey = 'VDYBN-27WPP-V4HQT-9VMD4-VMK7H'} #Windows Server 2022 Standard
-                    if ($selectServer -eq 4) {$productkey = 'CB7KF-BWN84-R7R2Y-793K2-8XDDG'} #Windows Server 2016 Datacenter
-                    if ($selectServer -eq 5) {$productkey = 'WMDGN-G9PQG-XVVXX-R3X43-63DFG'} #Windows Server 2019 Datacenter
-                    if ($selectServer -eq 6) {$productkey = 'WX4NM-KYWYW-QJJR4-XV3QB-6VM33'} #Windows Server 2022 Datacenter
+                    if ($selectServer -eq 1 -and $edition -match 'ServerStandard') {$productkey = 'WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY'} #Windows Server 2016 Standard
+                    if ($selectServer -eq 1 -and $edition -match 'ServerDatacenter') {$productkey = 'CB7KF-BWN84-R7R2Y-793K2-8XDDG'} #Windows Server 2016 Datacenter
+                    if ($selectServer -eq 2 -and $edition -match 'ServerStandard') {$productkey = 'N69G4-B89J2-4G8F4-WWYCC-J464C'} #Windows Server 2019 Standard
+                    if ($selectServer -eq 2 -and $edition -match 'ServerDatacenter') {$productkey = 'WMDGN-G9PQG-XVVXX-R3X43-63DFG'} #Windows Server 2019 Datacenter
+                    if ($selectServer -eq 3 -and $edition -match 'ServerStandard') {$productkey = 'VDYBN-27WPP-V4HQT-9VMD4-VMK7H'} #Windows Server 2022 Standard
+                    if ($selectServer -eq 3 -and $edition -match 'ServerDatacenter') {$productkey = 'WX4NM-KYWYW-QJJR4-XV3QB-6VM33'} #Windows Server 2022 Datacenter
   
                     Switch ($selectServer)
                        {
                        1 {Invoke-Command $windowsserver}
                        2 {Invoke-Command $windowsserver}
                        3 {Invoke-Command $windowsserver}
-                       4 {Invoke-Command $windowsserver}
-                       5 {Invoke-Command $windowsserver}
-                       6 {Invoke-Command $windowsserver}
                        }
                     }
-                    While ($selectServer -ne 7)
+                    While ($selectServer -ne 4)
                     cls
             }
         }
