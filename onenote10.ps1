167ad024b7a5db122e27dc8597e1f841c51b6d0d
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-07-18 14:11:17 +0700

    Update onenote10.ps1

--- onenote10.ps1
+++ onenote10.ps1
@@ -1,3 +1,10 @@
+#Install and update Desktop framework packages
+Set-Location $env:temp
+$uri = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+Invoke-WebRequest -Uri $uri -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+Add-AppxPackage -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+
+#Download and Install OneNote for Windows 10
 $uri = 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Files/Microsoft.Office.OneNote_16001.14326.21146.0.AppxBundle'
 Write-Host
 Write-Host 'Downloading Microsoft OneNote Package...'
