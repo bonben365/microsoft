5cb68d5e42edbb6d97645eddc431d8310d046774
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-07-18 13:42:45 +0700

    Update onenote10.ps1

--- onenote10.ps1
+++ onenote10.ps1
@@ -1,7 +1,7 @@
 $uri = 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Files/Microsoft.Office.OneNote_16001.14326.21146.0.AppxBundle'
-(New-Object Net.WebClient).DownloadFile($uri, "$env:temp\Microsoft.Office.OneNote_16001.14326.21146.0.AppxBundle")
 Write-Host
 Write-Host 'Downloading Microsoft OneNote Package...'
+(New-Object Net.WebClient).DownloadFile($uri, "$env:temp\Microsoft.Office.OneNote_16001.14326.21146.0.AppxBundle")
 Add-AppxPackage -Path "$env:temp\Microsoft.Office.OneNote_16001.14326.21146.0.AppxBundle"
 Write-Host
 Write-Host 'Installed Package:'
