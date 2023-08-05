d127219e889c2fa7a938558e0e49f09b1b82d6ba
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-08-05 10:41:18 +0700

    Update wato.ps1

--- wato.ps1
+++ wato.ps1
@@ -9,14 +9,16 @@ Write-Host For detailed script execution: https://msgang.com/wato
 Write-Host ===============================================================
 
 $edition = (Get-CimInstance Win32_OperatingSystem).Caption
-Write-Host '----------------------------------------'
-Write-Host "You're using $edition"                 -
-Write-Host '----------------------------------------'
+Write-Host '--------------------------------------------------------------'
+Write-Host "You're using $edition"                 
+Write-Host '--------------------------------------------------------------'
 
 If ($edition -eq 'Microsoft Windows 11 Pro' -or $edition -eq 'Microsoft Windows 10 Pro') {$productkey = 'W269N-WFGWX-YVC9B-4J6C9-T83GX'}
 If ($edition -eq 'Microsoft Windows 11 Enterprise' -or $edition -eq 'Microsoft Windows 10 Enterprise') {$productkey = 'NPPR9-FWDCX-D2C8J-H872K-2YT43'}
 If ($edition -eq 'Microsoft Windows 11 Education' -or $edition -eq 'Microsoft Windows 10 Education') {$productkey = 'NW6C2-QMPVW-D7KKK-3GKT6-VCFB2'}
-If ($edition -eq 'Microsoft Windows 11 Education' -or $edition -eq 'Microsoft Windows 10 Pro for Workstations') {$productkey = 'NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J'}
+If ($edition -eq 'Microsoft Windows 11 Education' -or $edition -eq 'Microsoft Windows 10 Education') {$productkey = 'NW6C2-QMPVW-D7KKK-3GKT6-VCFB2'}
+
+If ($edition -eq 'Microsoft Windows 11 Education' -or $edition -eq 'Microsoft Windows 10 Home') {$productkey = 'TX9XD-98N7V-6WMQ6-BX7FG-H8Q99'}
 
 
 
