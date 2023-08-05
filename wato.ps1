f710fede4e6476d94f8b6e1ed53e43ea9101ecd2
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-08-05 09:43:24 +0700

    Update wato.ps1

--- wato.ps1
+++ wato.ps1
@@ -12,23 +12,12 @@ $edition = (Get-CimInstance Win32_OperatingSystem).Caption
 Write-Host '-----------------------------------'
 Write-Host "You're using $edition"
 Write-Host '-----------------------------------'
-If ($edition -eq 'Microsoft Windows 11 Pro' -or $edition -eq 'Microsoft Windows 10 Pro') {
-  cscript $env:windir\system32\slmgr.vbs /ckms
-  cscript $env:windir\system32\slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
-  cscript $env:windir\system32\slmgr.vbs /skms kms.msgang.com
-  cscript $env:windir\system32\slmgr.vbs /ato -ForegroundColor Yellow
-}
 
-If ($edition -eq 'Microsoft Windows 11 Enterprise' -or $edition -eq 'Microsoft Windows 10 Enterprise') {
-  cscript $env:windir\system32\slmgr.vbs /ckms
-  cscript $env:windir\system32\slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
-  cscript $env:windir\system32\slmgr.vbs /skms kms.msgang.com
-  cscript $env:windir\system32\slmgr.vbs /ato
-}
+If ($edition -eq 'Microsoft Windows 11 Pro' -or $edition -eq 'Microsoft Windows 10 Pro') {$productkey = 'W269N-WFGWX-YVC9B-4J6C9-T83GX'}
+If ($edition -eq 'Microsoft Windows 11 Enterprise' -or $edition -eq 'Microsoft Windows 10 Enterprise') {$productkey = 'NPPR9-FWDCX-D2C8J-H872K-2YT43'}
+If ($edition -eq 'Microsoft Windows 11 Education' -or $edition -eq 'Microsoft Windows 10 Education') {$productkey = 'NW6C2-QMPVW-D7KKK-3GKT6-VCFB2'}
 
-If ($edition -eq 'Microsoft Windows 11 Education' -or $edition -eq 'Microsoft Windows 10 Education') {
-  cscript $env:windir\system32\slmgr.vbs /ckms
-  cscript $env:windir\system32\slmgr.vbs /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
-  cscript $env:windir\system32\slmgr.vbs /skms kms.msgang.com
-  cscript $env:windir\system32\slmgr.vbs /ato
-}
+cscript $env:windir\system32\slmgr.vbs /ckms
+cscript $env:windir\system32\slmgr.vbs /ipk "$productkey"
+cscript $env:windir\system32\slmgr.vbs /skms kms.msgang.com
+cscript $env:windir\system32\slmgr.vbs /ato
