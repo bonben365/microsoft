9e556db6438e845c85efe82dab5cba163e15b84c
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-08-05 08:25:05 +0700

    Update wato.ps1

--- wato.ps1
+++ wato.ps1
@@ -1,7 +1,7 @@
 $edition = (Get-CimInstance Win32_OperatingSystem).Caption
-Write-Host
+Write-Host '-----------------------------------'
 Write-Host "You're using $edition"
-Write-Host
+Write-Host '-----------------------------------'
 If ($edition -eq 'Microsoft Windows 11 Pro' -or $edition -eq 'Microsoft Windows 10 Pro') {
   cscript $env:windir\system32\slmgr.vbs /ckms
   cscript $env:windir\system32\slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
