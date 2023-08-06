ff1285bd2f8725bc6027fdf97f57a62e39c6350e
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-08-06 07:28:33 +0700

    Update oatc.ps1

--- oatc.ps1
+++ oatc.ps1
@@ -88,38 +88,43 @@ foreach ($key in $keys) {
     cscript ospp.vbs /unpkey:$key | Out-Null
 }
 
-$productkeys = @(
-    'NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP';
-    '6NWWJ-YQWMR-QKGCB-6TMB3-9D9HK';
-    'B4NPR-3FKK7-T2MBV-FRQ4W-PKD2B';
-    'C4F7P-NCP8C-6CQPT-MQHV9-JXD2M';
-    '9BGNQ-K37YR-RQHF2-38RQ3-7VCBB';
-    '7TQNQ-K3YQQ-3PFH7-CCPPM-X4VQ2';
-    '9N9PT-27V4Y-VJ2PD-YXFMF-YTFQT';
-    'TMJWT-YYNMB-3BKTF-644FC-RVXBD';
-    '7HD7K-N4PVK-BHBCQ-YWQRW-XW4VK';
-    'RRNCX-C64HY-W2MM7-MCH9G-TJHMQ';
-    'G2KWX-3NW6P-PY93R-JXK2T-C9Y9V';
-    'NCJ33-JHBBY-HTK98-MYCV8-HMKHJ';
-    'PBX3G-NWMT6-Q7XBW-PYJGG-WXD33';
-    'FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH';
-    'KDX7X-BNVR8-TXXGX-4Q7Y8-78VT3';
-    'FTNWT-C6WBT-8HMGF-K9PRX-QV9H8';
-    'J2JDC-NJCYY-9RGQ4-YXWMH-T3D4T';
-    'KNH8D-FGHT4-T8RK3-CTDYJ-K2HT4';
-    'MJVNY-BYWPY-CWV6J-2RKRT-4M8QG';
-    'WM8YG-YNGDD-4JHDC-PG3F4-FC4T4';
-    'NWG3X-87C9K-TC7YY-BC2G7-G6RVC';
-    'C9FM6-3N72F-HFJXB-TM3V9-T86R9';
-    'TY7XF-NFRBR-KJ44C-G83KF-GX27K';
-    '2MW9D-N4BXM-9VBPG-Q7W6M-KFBGQ';
-    'HWCXN-K3WBT-WJBKY-R8BD9-XK29P';
-    'TN8H9-M34D3-Y64V9-TR72V-X79KV')
+if (($dstatus | Select-String -SimpleMatch "Office19").Count -gt 0) {
+    $productkeys = @(
+        'NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP';
+        '6NWWJ-YQWMR-QKGCB-6TMB3-9D9HK';
+        'B4NPR-3FKK7-T2MBV-FRQ4W-PKD2B';
+        'C4F7P-NCP8C-6CQPT-MQHV9-JXD2M';
+        '9BGNQ-K37YR-RQHF2-38RQ3-7VCBB';
+        '7TQNQ-K3YQQ-3PFH7-CCPPM-X4VQ2';
+        '9N9PT-27V4Y-VJ2PD-YXFMF-YTFQT';
+        'TMJWT-YYNMB-3BKTF-644FC-RVXBD';
+        '7HD7K-N4PVK-BHBCQ-YWQRW-XW4VK';
+        'RRNCX-C64HY-W2MM7-MCH9G-TJHMQ';
+        'G2KWX-3NW6P-PY93R-JXK2T-C9Y9V';
+        'NCJ33-JHBBY-HTK98-MYCV8-HMKHJ';
+        'PBX3G-NWMT6-Q7XBW-PYJGG-WXD33';
+        'FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH';
+        'KDX7X-BNVR8-TXXGX-4Q7Y8-78VT3';
+        'FTNWT-C6WBT-8HMGF-K9PRX-QV9H8';
+        'J2JDC-NJCYY-9RGQ4-YXWMH-T3D4T';
+        'KNH8D-FGHT4-T8RK3-CTDYJ-K2HT4';
+        'MJVNY-BYWPY-CWV6J-2RKRT-4M8QG';
+        'WM8YG-YNGDD-4JHDC-PG3F4-FC4T4';
+        'NWG3X-87C9K-TC7YY-BC2G7-G6RVC';
+        'C9FM6-3N72F-HFJXB-TM3V9-T86R9';
+        'TY7XF-NFRBR-KJ44C-G83KF-GX27K';
+        '2MW9D-N4BXM-9VBPG-Q7W6M-KFBGQ';
+        'HWCXN-K3WBT-WJBKY-R8BD9-XK29P';
+        'TN8H9-M34D3-Y64V9-TR72V-X79KV')
 
-foreach ($productkey in $productkeys) {
-    cscript ospp.vbs /inpkey:$productkey | Out-Null
+    foreach ($productkey in $productkeys) {
+        cscript ospp.vbs /inpkey:$productkey | Out-Null
+    }
+    cscript ospp.vbs /sethst:kms.msgang.com | Out-Null
+    cscript ospp.vbs /act
+
 }
-cscript ospp.vbs /sethst:kms.msgang.com | Out-Null
-cscript ospp.vbs /act
+
+
 
 
