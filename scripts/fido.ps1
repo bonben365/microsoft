28c0e48d81f1945db662ed567fe2442723144480
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-08-15 13:55:38 +0700

    Update fido.ps1

--- scripts/fido.ps1
+++ scripts/fido.ps1
@@ -1,4 +1,4 @@
-#Write-Host ===============================================================
+#Write-Host ***************************************************************
 #Write-Host Name:           Windows Download PowerShell Script
 #Write-Host Description:    Download the lastest versions.
 #Write-Host Version:        1.0
@@ -6,7 +6,7 @@
 #Write-Host Website:        https://bonguides.com
 #Write-Host Script by:      Leo Nguyen
 #Write-Host For detailed script execution: https://bonguides.com
-#Write-Host ===============================================================
+#Write-Host ***************************************************************
 
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
@@ -20,8 +20,18 @@ Set-ExecutionPolicy Bypass Process
 
 Invoke-RestMethod 'https://raw.githubusercontent.com/pbatard/Fido/master/Fido.ps1' -OutFile "$env:temp\tmp\fido.ps1"
 
+Write-Host
 .\fido.ps1 -win list
 
 Write-Host
-Write-Host "Donwload Windows 10: .\fido.ps1 -win 10"
-Write-Host "Donwload Windows 11: .\fido.ps1 -win 11"
+Write-Host "*Instructions.................................................."    -ForegroundColor Green
+Write-Host
+Write-Host "- Donwload Windows 10:        .\fido.ps1 -win 10"                   -ForegroundColor Yellow
+Write-Host "- Donwload Windows 11:        .\fido.ps1 -win 11"                   -ForegroundColor Yellow
+Write-Host
+Write-Host "- Get supported languages:    .\fido.ps1 -win 10 -lang list"        -ForegroundColor Yellow
+Write-Host "- Download custom language:   .\fido.ps1 -win 10 -lang Spanish"     -ForegroundColor Yellow
+Write-Host
+Write-Host "- Get download URL:           .\fido.ps1 -win 10 -GetURL"           -ForegroundColor Yellow
+Write-Host
+Write-host "*.............................................................."    -ForegroundColor Green
