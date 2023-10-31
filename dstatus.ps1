0bc7eb31325e59e84a20fbb663c77b48cea14cf9
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-11-01 01:12:56 +0700

    Create dstatus.ps1

---
+++ dstatus.ps1
@@ -0,0 +1,8 @@
+if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    break
+}
+
+$path = "C:\Program Files*\Microsoft Office\Office16\ospp.vbs"
+$ospp = Resolve-Path -Path $path | Select-Object -ExpandProperty Path -Last 1
+cscript $ospp /dstatus
