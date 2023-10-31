52f32251e703d2ef45a6dba85ab6addab4beec8a
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-11-01 01:31:56 +0700

    Update officeremkeys.ps1

--- officeremkeys.ps1
+++ officeremkeys.ps1
@@ -4,8 +4,8 @@ if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdenti
 }
 #Find OSPP.vbs path and run the command with the dstatus option (Last 1...)
 $OSPP = Resolve-Path -Path "C:\Program Files*\Microsoft Office\Office16\ospp.vbs" | Select-Object -ExpandProperty Path -Last 1
-Write-Output -InputObject "OSPP Location is: $OSPP" -ForegroundColor Green
-$Command = "cscript.exe '$OSPP' /dstatus"
+Write-Host "OSPP Location is: $OSPP" -ForegroundColor Green
+$Command = "cscript.exe //nologo '$OSPP' /dstatus"
 $DStatus = Invoke-Expression -Command $Command
 #Get product keys from OSPP.vbs output.
 $ProductKeys = $DStatus | Select-String -SimpleMatch "Last 5" | ForEach-Object -Process { $_.tostring().split(" ")[-1]}
@@ -14,7 +14,7 @@ if ($ProductKeys) {
     #Run OSPP.vbs per key with /unpkey option.
     foreach ($ProductKey in $ProductKeys) {
         Write-Host "Processing productkey $ProductKey" -ForegroundColor Green
-        $Command = "cscript.exe '$OSPP' /unpkey:$ProductKey"
+        $Command = "cscript.exe //nologo '$OSPP' /unpkey:$ProductKey"
         Invoke-Expression -Command $Command
     }
 } else {
