b0ef402b81709a4871350fda1f1cd278a8a130f2
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-11-03 10:09:18 +0700

    Update dstatus.ps1

--- dstatus.ps1
+++ dstatus.ps1
@@ -6,3 +6,10 @@ if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdenti
 $path = "C:\Program Files*\Microsoft Office\Office1*\ospp.vbs"
 $ospp = Resolve-Path -Path $path | Select-Object -ExpandProperty Path -Last 1
 cscript //nologo $ospp /dstatus
+
+
+$path64 = "C:\Program Files\Microsoft Office\Office1*"
+$path32 = "C:\Program Files (x86)\Microsoft Office\Office1*"
+if ((Test-Path -Path "$path32\ospp.vbs")) { 
+Set-Location $path32 -ErrorAction SilentlyContinue }
+if ((Test-Path -Path "$path64\ospp.vbs")) { Set-Location $path64 -ErrorAction SilentlyContinue }
