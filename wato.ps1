04cee50069e8d3ef2549d16a912c64fbd557c84e
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-11-18 07:57:43 +0700

    Update wato.ps1

--- wato.ps1
+++ wato.ps1
@@ -7,6 +7,7 @@ Write-Host =====================================================================
 
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm msgang.com/dl | iex"
     break
 }
 
