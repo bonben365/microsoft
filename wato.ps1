32c1b848f42d305cb6965cbe4d99a6bb56277fc2
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-11-18 08:38:49 +0700

    Update wato.ps1

--- wato.ps1
+++ wato.ps1
@@ -1,13 +1,13 @@
-Write-Host ========================================================================================
+<# Write-Host ========================================================================================
 Write-Host Description:    Activating Microsoft software products for FREE without additional software
 Write-Host Website:        https://msgang.com
 Write-Host Script by:      Leo Nguyen
 Write-Host For detailed script execution: https://msgang.com/windows
-Write-Host ========================================================================================
+Write-Host ======================================================================================== #>
 
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
-    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm msgang.com/dl | iex"
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm msgang.com/win | iex"
     break
 }
 
