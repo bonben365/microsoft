da51e2ed82625f5c33dbc8817d3383dfb4d741d2
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-11-20 13:05:00 +0700

    Update wato.ps1

--- wato.ps1
+++ wato.ps1
@@ -77,6 +77,11 @@ If ($edition -eq 'Microsoft Windows 8.1 Professional N') {$productkey = 'HMCNV-V
 If ($edition -eq 'Microsoft Windows 8.1 Enterprise') {$productkey = 'MHF9N-XY6XB-WVXMC-BTDCT-MKKG7'
 If ($edition -eq 'Microsoft Windows 8.1 Enterprise N') {$productkey = 'TT4HM-HN7YT-62K67-RGRQJ-JFFXW'
 
+If ($edition -eq 'Microsoft Windows 8 Professional') {$productkey = 'NG4HW-VH26C-733KW-K6F98-J8CK4'
+If ($edition -eq 'Microsoft Windows 8 Professional N') {$productkey = 'XCVCF-2NXM9-723PB-MHCB7-2RYQQ'
+If ($edition -eq 'Microsoft Windows 8 Enterprise') {$productkey = '	32JNW-9KQ84-P47T8-D8GGY-CWCK7'
+If ($edition -eq 'Microsoft Windows 8 Enterprise N') {$productkey = 'JMNMF-RHW7P-DMY6X-RF3DR-X2BQT'
+
 If ($edition7 -eq 'Microsoft Windows 10 Enterprise LTSC 2019' -or $edition7 -eq 'Microsoft Windows 10 Enterprise LTSC 2021') {$productkey = 'M7XTQ-FN8P6-TTKYV-9D4CC-J462D'}
 If ($edition7 -eq 'Microsoft Windows 10 Enterprise LTSB 2016') {$productkey = 'DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ'}
 If ($edition7 -eq 'Microsoft Windows 10 Enterprise 2015 LTSB') {$productkey = 'WNMTR-4C88C-JK8YV-HQ7T2-76DF9'}
