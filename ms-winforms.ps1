57a190751132b42a819e0add24e4acb4b271cf7c
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-12-19 04:02:55 +0700

    Update ms-winforms.ps1

--- ms-winforms.ps1
+++ ms-winforms.ps1
@@ -11,12 +11,12 @@ Script Highlights:
 
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
-    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm msgang.com/ms | iex"
     break
 }
 
-Add-Type -AssemblyName System.Windows.Forms, System.Drawing
-[System.Windows.Forms.Application]::EnableVisualStyles()
+# Load .NET assemblies into Windows PowerShell
+    Add-Type -AssemblyName System.Windows.Forms, System.Drawing
+    [System.Windows.Forms.Application]::EnableVisualStyles()
 
 # Long running task with runspace
 function MicrosoftWindowsAct {
